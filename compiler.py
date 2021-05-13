65018cb5499dd64657ccd9beea13adea106bd6a9
Koki Ryu <liukoki@gmail.com>
2021-05-14 00:09:48 +0900

    Complete compiler

--- compiler.py
+++ compiler.py
@@ -6,6 +6,7 @@ from classes import *
 SEP_CHARS = ["(", ")", "[", "]", "<", ">", "\"", ",", "{", "}", ":"]
 ARROW_MLIR = "->"
 ARROW_TOKEN = "$"
+INDEX_VARIABLES = ["i", "j", "k", "l", "m"]
 
 
 def lex(content):
@@ -305,7 +306,7 @@ def parse_global(tokens, idx):
         raise ParserError(newidx)
     newidx += 1
 
-    value, newidx = parse_value(tokens, newidx)
+    value, newidx = parse_value(tokens, newidx, shape)
     if tokens[newidx] != ":":
         raise ParserError(newidx)
     newidx += 1
@@ -355,7 +356,7 @@ def parse_int_list(tokens, idx):
     else:
         return [hd], idx + 1
 
-def parse_value(tokens, idx):
+def parse_value(tokens, idx, shape):
     if tokens[idx] != "value":
         raise ParserError(idx)
     if tokens[idx + 1] != "=":
@@ -364,12 +365,14 @@ def parse_value(tokens, idx):
         raise ParserError(idx + 2)
     if tokens[idx + 3] != "<":
         raise ParserError(idx + 3)
-    nparray, newidx = parse_nparray(tokens, idx + 4)
+    nparray, newidx = parse_nparray(tokens, idx + 4, shape)
     if tokens[newidx] != ">":
         raise ParserError(newidx)
     return nparray, newidx + 1
 
-def parse_nparray(tokens, idx):
+def parse_nparray(tokens, idx, shape):
+    if tokens[idx] == '"':
+        return parse_buffer_nparray(tokens, idx, shape)
     if tokens[idx] != "[":
         raise ParserError(idx)
     idx += 1
@@ -383,6 +386,18 @@ def parse_nparray(tokens, idx):
             paren_count -= 1
         idx += 1
     return np.array(ast.literal_eval(literal), dtype=np.float32), idx
+
+def parse_buffer_nparray(tokens, idx, shape):
+    if tokens[idx] != '"':
+        raise ParserError(idx)
+    if len(shape) != 2:
+        raise RuntimeError("Non-2d buffer array is not supported")
+    buf_bytes = bytes.fromhex(tokens[idx + 1][2:])
+    array = np.frombuffer(buf_bytes, dtype=np.float32).reshape(shape)
+    if tokens[idx + 2] != '"':
+        raise ParserError(idx)
+    return array, idx + 3
+
 
 def parse_for(tokens, idx):
     if tokens[idx] != "affine.for":
@@ -616,12 +631,261 @@ def parse_return(tokens, idx):
     memref, newidx = parse_type(tokens, idx + 3)
     return ReturnOp(var, memref), newidx
 
+memory_types = {}
+
+def print_prelude():
+    print("#include <ap_int.h>")
+    print("#include <hls_stream.h>")
+    print("#include <ap_axi_sdata.h>")
+    print("#include <math.h>")
+    print("using namespace hls;")
+    print("typedef ap_axiu<32, 0, 0, 0> AP_AXIS;")
+    print("typedef stream<AP_AXIS> AXI_STREAM;")
+    print()
+
+def shape_brackets(shape):
+    ret = ""
+    for i in shape:
+        ret += "[" + str(i) + "]"
+    return ret
+def val_type_str(val_type):
+    if isinstance(val_type, F32):
+        return "float"
+    if isinstance(val_type, I64):
+        return "long"
+    if isinstance(val_type, I8):
+        return "char"
+    return "int"
+
+def memref_type(memref):
+    return val_type_str(memref.base_type)
+
+def print_memref_var(var_name, memref, tabs=""):
+    brackets = shape_brackets(memref.size)
+    type_name = memref_type(memref)
+    print(f"{tabs}{type_name} {var_name}{brackets};")
+
+def print_input(var_name, memref, tabs=""):
+    print(f"{tabs}AP_AXIS element;")
+    print()
+    for i in range(len(memref.size)):
+        var = INDEX_VARIABLES[i]
+        linetabs = tabs + '\t' * i
+        s = f"{linetabs}for (int {var} = 0; {var} < {memref.size[i]}; {var}++) {{"
+        print(s)
+    linetabs = tabs + '\t' * len(memref.size)
+    print(f"{linetabs}element = in_strm.read();")
+    print(f"{linetabs}my_converter.as_uint32 = element.data;")
+
+    print(f"{linetabs}{var_name}{shape_brackets(INDEX_VARIABLES[:len(memref.size)])} = my_converter.as_floatingpoint;")
+
+    for i in range(len(memref.size)):
+        linetabs = tabs + '\t' * (len(memref.size) - 1 - i)
+        print(f"{linetabs}}}")
+
+def print_f32_const(var_name, exp, tabs):
+    if exp.val == np.inf:
+        exp_str = "INFINITY"
+    elif exp.val == -np.inf:
+        exp_str = "-INFINITY"
+    else:
+        exp_str = str(exp.val)
+    print(f"{tabs}float {var_name} = {exp_str};")
+
+def print_i64_const(var_name, exp, tabs):
+    print(f"{tabs}long {var_name} = {exp.val};")
+
+def print_getref(var_name, exp, tabs):
+    memref = exp.getref_type.rets
+    memory_types[var_name] = memref
+    print(f"{tabs}{memref_type(memref)} {var_name}{shape_brackets(memref.size)};")
+
+def stringify_numpy_array(ndarray):
+    if isinstance(ndarray, np.ndarray):
+        return "{" + ",".join([stringify_numpy_array(n) for n in ndarray]) + "}"
+    else:
+        return str(ndarray)
+
+def print_global(var_name, exp, tabs):
+    tensor_str = stringify_numpy_array(exp.value)
+    memref = exp.global_type.rets
+    memory_types[var_name] = memref
+    print(f"{tabs}{memref_type(memref)} {var_name}{shape_brackets(memref.size)} = {tensor_str};")
+
+def print_alloc(var_name, exp, tabs):
+    memref = exp.memref
+    memory_types[var_name] = memref
+    print(f"{tabs}{memref_type(memref)} {var_name}{shape_brackets(memref.size)};")
+
+def print_alloca(var_name, exp, tabs):
+    memref = exp.memref
+    memory_types[var_name] = memref
+    print(f"{tabs}{memref_type(memref)} {var_name}{shape_brackets(memref.size)};")
+
+def print_load(var_name, exp, tabs):
+    memref = exp.memref
+    mem_idx_vars = ["x" + v for v in exp.mem_idx]
+    mem_var_name = "x" + exp.mem_var
+    print(f"{tabs}{memref_type(memref)} {var_name} = {mem_var_name}{shape_brackets(mem_idx_vars)};")
+
+def print_mulf(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}float {var_name} = {var_name1} * {var_name2};")
+
+def print_addf(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}float {var_name} = {var_name1} + {var_name2};")
+
+def print_divf(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}float {var_name} = {var_name1} / {var_name2};")
+
+def print_subf(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}float {var_name} = {var_name1} - {var_name2};")
+
+def print_subf(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}float {var_name} = {var_name1} - {var_name2};")
+
+def print_exp(var_name, exp, tabs):
+    arg_name = "x" + exp.var
+    print(f"{tabs}float {var_name} = exp({arg_name});")
+
+def print_gt(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}bool {var_name} = {var_name1} > {var_name2};")
+
+def print_lt(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    print(f"{tabs}bool {var_name} = {var_name1} < {var_name2};")
+
+def print_select(var_name, exp, tabs):
+    var_name1 = "x" + exp.var1
+    var_name2 = "x" + exp.var2
+    var_name3 = "x" + exp.var3
+    print(f"{tabs}{val_type_str(exp.val_type)} {var_name} = {var_name1} ? {var_name2} : {var_name3};")
+
+def print_subst(subst_op, tabs):
+    var_name = "x" + subst_op.var_name
+    exp = subst_op.exp
+    if isinstance(exp, F32Const):
+        print_f32_const(var_name, exp, tabs)
+    elif isinstance(exp, I64Const):
+        print_i64_const(var_name, exp, tabs)
+    elif isinstance(exp, GetRef):
+        print_getref(var_name, exp, tabs)
+    elif isinstance(exp, Alloc):
+        print_alloc(var_name, exp, tabs)
+    elif isinstance(exp, Global):
+        print_global(var_name, exp, tabs)
+    elif isinstance(exp, Alloca):
+        print_alloca(var_name, exp, tabs)
+    elif isinstance(exp, Load):
+        print_load(var_name, exp, tabs)
+    elif isinstance(exp, Mulf):
+        print_mulf(var_name, exp, tabs)
+    elif isinstance(exp, Addf):
+        print_addf(var_name, exp, tabs)
+    elif isinstance(exp, Divf):
+        print_divf(var_name, exp, tabs)
+    elif isinstance(exp, Subf):
+        print_subf(var_name, exp, tabs)
+    elif isinstance(exp, Exp):
+        print_exp(var_name, exp, tabs)
+    elif isinstance(exp, Gt):
+        print_gt(var_name, exp, tabs)
+    elif isinstance(exp, Lt):
+        print_lt(var_name, exp, tabs)
+    elif isinstance(exp, Select):
+        print_select(var_name, exp, tabs)
+
+def print_for(op, tabs):
+    arg_name = "x" + op.arg_name
+    print(f"{tabs}for (int {arg_name} = {op.loop_range[0]}; {arg_name} < {op.loop_range[1]}; {arg_name}++) {{")
+    print_block(op.block, tabs + "\t")
+    print(f"{tabs}}}")
+
+def print_store(op, tabs):
+    val_var_name = "x" + op.val_var
+    mem_var_name = "x" + op.mem_var
+    mem_idx_vars = ["x" + v for v in op.mem_idx]
+    print(f"{tabs}{mem_var_name}{shape_brackets(mem_idx_vars)} = {val_var_name};")
+
+def loop_termination(memref_size):
+    return "&&".join([INDEX_VARIABLES[i] + " == " + str(memref_size[i] - 1) for i in range(len(memref_size))])
+
+def print_return(op, tabs):
+    print(f"{tabs}AP_AXIS val;")
+    print(f"{tabs}val.keep = element.keep;")
+    print(f"{tabs}val.strb = element.strb;")
+    print(f"{tabs}val.last = 0;")
+    print()
+    var_name = "x" + op.var
+    memref = memory_types[var_name]
+
+    for i in range(len(memref.size)):
+        var = INDEX_VARIABLES[i]
+        linetabs = tabs + '\t' * i
+        s = f"{linetabs}for (int {var} = 0; {var} < {memref.size[i]}; {var}++) {{"
+        print(s)
+    linetabs = tabs + '\t' * len(memref.size)
+
+    print(f"{linetabs}my_converter.as_floatingpoint = {var_name}{shape_brackets(INDEX_VARIABLES[:len(memref.size)])};")
+    print(f"{linetabs}val.data = my_converter.as_uint32;")
+    print(f"{linetabs}if({loop_termination(memref.size)}) val.last = 1;")
+    print(f"{linetabs}out_strm << val;")
+
+    for i in range(len(memref.size)):
+        linetabs = tabs + '\t' * (len(memref.size) - 1 - i)
+        print(f"{linetabs}}}")
+
+    print(f"{tabs}return;")
+
+def print_op(op, tabs):
+    if isinstance(op, SubstOp):
+        print_subst(op, tabs)
+    elif isinstance(op, ForOp):
+        print_for(op, tabs)
+    elif isinstance(op, StoreOp):
+        print_store(op, tabs)
+    elif isinstance(op, ReturnOp):
+        print_return(op, tabs)
+
+def print_block(block, tabs=""):
+    for op in block:
+        print_op(op, tabs)
+
+def print_vitis_module(func):
+    print_prelude()
+    print("void graph(AXI_STREAM &in_strm, AXI_STREAM &out_strm) {")
+    print_memref_var("x" + func.args[0][0], func.args[0][1], tabs="\t")
+    print("\t#pragma HLS INTERFACE s_axilite port=return bundle=CONTROL_BUS")
+    print("\t#pragma HLS INTERFACE axis port=in_strm")
+    print("\t#pragma HLS INTERFACE axis port=out_strm")
+    print("\tunion")
+    print("\t{")
+    print("\t    unsigned int as_uint32;")
+    print("\t    float as_floatingpoint;")
+    print("\t} my_converter;")
+    print_input("x" + func.args[0][0], func.args[0][1], tabs="\t")
+    print_block(func.block, tabs="\t")
+    print("}")
+
+
+
 filename = sys.argv[1]
 with open(filename) as f: content = f.read()
 tokens = lex(content)
 module_ast, idx = parse_module(tokens, 0)
-
-breakpoint()
+print_vitis_module(module_ast)
 
 
 
